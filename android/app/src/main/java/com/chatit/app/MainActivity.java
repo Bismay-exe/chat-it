cca5ab2ff3288be74c7c2fb5ddf114f2b2e716ea
Bismay.exe <bismaysundarmahanta@gmail.com>
2026-04-10 02:29:33 +0530

    chat-it

--- android/app/src/main/java/com/chatit/app/MainActivity.java
+++ android/app/src/main/java/com/chatit/app/MainActivity.java
@@ -1,12 +1,16 @@
 package com.chatit.app;
 
 import android.os.Bundle;
+import androidx.core.view.WindowCompat;
 import androidx.core.splashscreen.SplashScreen;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Force edge-to-edge
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
     }
