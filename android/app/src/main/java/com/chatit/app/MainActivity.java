0fbc577b41b9e71b10ffd7046bb31014e6dc85b9
Bismay.exe <bismaysundarmahanta@gmail.com>
2026-04-10 22:01:42 +0530

    chat-it

--- android/app/src/main/java/com/chatit/app/MainActivity.java
+++ android/app/src/main/java/com/chatit/app/MainActivity.java
@@ -2,6 +2,7 @@ package com.chatit.app;
 
 import android.os.Bundle;
 import androidx.core.splashscreen.SplashScreen;
+import androidx.core.view.WindowCompat;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
@@ -9,5 +10,9 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
+
+        // Enable true edge-to-edge: content draws behind status bar & nav bar
+        // This removes the default semi-transparent scrim Android adds
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
     }
 }
